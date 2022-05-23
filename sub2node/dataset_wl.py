89164674255e3f515762378dd3020132175fb82c
Dongkwan Kim <todoaskit@gmail.com>
2022-05-23 23:11:40 +0900

    Add dataset_wl

---
+++ sub2node/dataset_wl.py
@@ -0,0 +1,152 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import sklearn.preprocessing as skp
+import torch
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from torch import Tensor
+from torch_geometric.data import Data
+from torch_geometric.nn import WLConv
+from torch_geometric.typing import Adj
+from torch_geometric.utils import to_undirected, to_networkx, from_networkx
+from torch_geometric.utils.num_nodes import maybe_num_nodes
+from torch_sparse import SparseTensor
+
+torch.manual_seed(42)
+
+
+class WL4PatternConv(WLConv):
+
+    def __init__(self):
+        super().__init__()
+
+    @torch.no_grad()
+    def forward(self, x: Tensor, edge_index: Adj) -> Tensor:
+        if x.dim() > 1:
+            assert (x.sum(dim=-1) == 1).sum() == x.size(0)
+            x = x.argmax(dim=-1)  # one-hot -> integer.
+        assert x.dtype == torch.long
+
+        adj_t = edge_index
+        if not isinstance(adj_t, SparseTensor):
+            adj_t = SparseTensor(row=edge_index[1], col=edge_index[0],
+                                 sparse_sizes=(x.size(0), x.size(0)))
+
+        out = []
+        _, col, _ = adj_t.coo()
+        deg = adj_t.storage.rowcount().tolist()
+        for node, neighbors in zip(x.tolist(), x[col].split(deg)):
+            # Use idx without hash(.)
+            # idx = hash(tuple([node] + neighbors.sort()[0].tolist()))
+            idx = tuple([node] + neighbors.sort()[0].tolist())
+            if idx not in self.hashmap:
+                self.hashmap[idx] = len(self.hashmap)
+
+            out.append(self.hashmap[idx])
+
+        return torch.tensor(out, device=x.device)
+
+    def color_pattern(self, color: Tensor, outtype="bow") -> Tensor:
+        color_to_pattern = {v: k for k, v in self.hashmap.items()}
+        assert len(self.hashmap) == len(color_to_pattern)
+
+        self_patterns, neighbor_patterns = [], []
+        for c in color.tolist():
+            sp, *np = color_to_pattern[c]
+            self_patterns.append([sp])
+            neighbor_patterns.append(np)
+
+        if outtype == "bow":
+            vectorizer = CountVectorizer(preprocessor=lambda _: _, tokenizer=lambda _: _)
+            pattern_transformed = vectorizer.fit_transform(self_patterns + neighbor_patterns)
+            # print(vectorizer.get_feature_names_out())
+            N = color.size(0)
+            pattern_vec = pattern_transformed.toarray()
+            sp_vec = torch.from_numpy(skp.normalize(pattern_vec[:N, :]))
+            np_vec = torch.from_numpy(skp.normalize(pattern_vec[N:, :]))
+            return torch.cat([sp_vec, np_vec], dim=-1)
+
+        else:
+            raise NotImplementedError
+
+    def color_pattern_cluster(self, color: Tensor,
+                              pattern_outtype="bow",
+                              clustering_name="KMeans", **kwargs) -> Tensor:
+        pattern_x = self.color_pattern(color, pattern_outtype)
+        assert clustering_name in ["KMeans"]
+        clustering = eval(clustering_name)(**kwargs).fit(pattern_x)
+        return torch.from_numpy(clustering.labels_).long()
+
+
+class WL4PatternNet(torch.nn.Module):
+
+    def __init__(self, num_layers, clustering_name="KMeans"):
+        super().__init__()
+        num_layers += 1  # todo: for the last layer's color_pattern analysis
+        self.convs = torch.nn.ModuleList([WL4PatternConv() for _ in range(num_layers)])
+
+        self.clustering_name = clustering_name
+        self.cluster_kwargs = {
+            "KMeans": {"n_clusters": 2},
+        }[self.clustering_name]
+
+    def forward(self, x, edge_index, batch=None):
+
+        colors, hists, clusters = [], [], []
+
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
+
+            if (i + 1) != len(self.convs):  # todo: not last layer
+                colors.append(x)
+                hists.append(conv.histogram(x, batch, norm=False))
+
+            if i != 0:  # todo: not first layer
+                clusters.append(conv.color_pattern_cluster(x, **self.cluster_kwargs))
+
+        return colors, hists, clusters
+
+
+def draw_graph_with_coloring(data: Data,
+                             colors: torch.Tensor,
+                             title: str):
+    g = to_networkx(data, to_undirected=True)
+
+    nodes = g.nodes()
+    colors = colors.tolist()
+
+    pos = nx.spring_layout(g, seed=0)  # or nx.shell_layout(g)
+    nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_nodes(g, pos, nodelist=nodes, node_color=colors,
+                           node_size=200, cmap=plt.cm.Set3)
+    nx.draw_networkx_labels(g, pos, labels=dict(zip(nodes, colors)),
+                            font_size=15)
+
+    ax = plt.gca()
+    ax.margins(0.20)
+    plt.title(title)
+    plt.axis("off")
+    plt.show()
+
+
+def draw_examples(edge_index, num_layers):
+    edge_index = to_undirected(edge_index.long())
+    data = Data(x=torch.ones(maybe_num_nodes(edge_index)).long(),
+                edge_index=edge_index)
+
+    wl = WL4PatternNet(num_layers=num_layers)
+    colors, hists, clusters = wl(data.x, data.edge_index, data.batch)
+
+    for i, (co, cl) in enumerate(zip(colors, clusters)):
+        draw_graph_with_coloring(data, co, title=f"WL color: {i + 1} steps")
+        draw_graph_with_coloring(data, cl, title=f"WL pattern-cluster: {i + 1} steps")
+
+
+if __name__ == '__main__':
+    # _g = nx.dorogovtsev_goltsev_mendes_graph(3)
+    # _g = nx.random_partition_graph([5, 5, 5], 0.6, 0.1, seed=10)
+    _g = nx.random_partition_graph([7, 7, 7, 7], 0.64, 0.1, seed=10)
+    draw_examples(
+        edge_index=from_networkx(_g).edge_index,
+        num_layers=3,
+    )
