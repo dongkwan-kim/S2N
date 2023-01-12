e3e52a05c29cbdcc0d2571540bcfd307f5e53cd5
Dongkwan Kim <todoaskit@gmail.com>
2023-01-13 01:14:45 +0900

    Finalize hparams_to_yaml

--- sub2node/hparams_to_yaml.py
+++ sub2node/hparams_to_yaml.py
@@ -28,6 +28,7 @@ def load_kwargs(dataset_name, batching_type, model_name) -> (str, DictConfig, st
         "HPOMetab": "hpo_metab",
         "EMUser": "em_user"
     })
+    dataset_name = dataset_name.split("-")[0]  # for cases like DATASET-[0.x, 0.y, 0.z]
     if batching_type == "s2n":
         datamodule_yaml = f"../configs/datamodule/s2n/{dataset_name}/for-{model_name}.yaml"
     else:
@@ -58,7 +59,8 @@ def load_best_hparams_per_dataset(dataset_name, log_path) -> List[Dict[str, Any]
     return hparams_list
 
 
-def replace_and_dump_hparams_to_args(dataset_name, log_path="../_aggr_logs"):
+def replace_and_dump_hparams_to_args(dataset_name, log_path="../_aggr_logs",
+                                     dump_datamodule=True, dump_model=True):
     best_hparams = load_best_hparams_per_dataset(dataset_name, log_path=log_path)
     for bh_dict in best_hparams:
         bh_datamodule, bh_model = bh_dict["datamodule"], bh_dict["model"]
@@ -93,22 +95,38 @@ def replace_and_dump_hparams_to_args(dataset_name, log_path="../_aggr_logs"):
             if k in bh_datamodule and bh_datamodule[k] != "":
                 datamodule_cfg[k] = bh_datamodule[k]
 
-        # NOTE: uncomment these lines to dump datamodule args.
-        # with open(datamodule_yaml, "w") as f:
-        #     OmegaConf.save(datamodule_cfg, f)
-        #     cprint(f"Saved: {datamodule_yaml}", "blue")
+        if dump_datamodule:
+            with open(datamodule_yaml, "w") as f:
+                OmegaConf.save(datamodule_cfg, f)
+                cprint(f"Saved: {datamodule_yaml}", "blue")
 
-        with open(model_yaml, "w") as f:
-            OmegaConf.save(model_cfg, f)
-            cprint(f"Saved: {model_yaml}", "blue")
+        if dump_model:
+            with open(model_yaml, "w") as f:
+                OmegaConf.save(model_cfg, f)
+                cprint(f"Saved: {model_yaml}", "blue")
 
 
 if __name__ == '__main__':
-    # ../_aggr_logs/for-ICML2023/main
-    # ../_aggr_logs/for-ICML2023/sensitivity
-    LOG_PATH = "../_aggr_logs/for-ICML2023/main"
+    LOG_PATH = "../_aggr_logs/for-ICML2023/sensitivity_v2"
 
-    replace_and_dump_hparams_to_args("PPIBP", LOG_PATH)
-    replace_and_dump_hparams_to_args("HPOMetab", LOG_PATH)
-    replace_and_dump_hparams_to_args("HPONeuro", LOG_PATH)
-    replace_and_dump_hparams_to_args("EMUser", LOG_PATH)
+    # ../_aggr_logs/for-ICML2023/main
+    if not "sensitivity" in LOG_PATH:
+        replace_and_dump_hparams_to_args("PPIBP", LOG_PATH)
+        replace_and_dump_hparams_to_args("HPOMetab", LOG_PATH)
+        replace_and_dump_hparams_to_args("HPONeuro", LOG_PATH)
+        replace_and_dump_hparams_to_args("EMUser", LOG_PATH)
+
+    # ../_aggr_logs/for-ICML2023/sensitivity_v2
+    else:
+
+        IDX = 0  # 0, 1, 2, 3
+
+        prefix_settings = {
+            "PPIBP": [0.1, 0.3, 0.5, 0.7],
+            "HPOMetab": [0.1, 0.3, 0.5, 0.7],
+            "HPONeuro": [0.1, 0.3, 0.5, 0.7],
+            "EMUser": [0.15, 0.3, 0.45, 0.6],
+        }
+        for _dataset_name, _prefix_list in prefix_settings.items():
+            _dataset_name_w_prefix = f"{_dataset_name}-[{_prefix_list[IDX]}"
+            replace_and_dump_hparams_to_args(_dataset_name_w_prefix, LOG_PATH)
