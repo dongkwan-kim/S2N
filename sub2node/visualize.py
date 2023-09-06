f44b700ea8230cba6ce686029707c78eef0099c3
Dongkwan Kim <todoaskit@gmail.com>
2023-09-07 00:10:17 +0900

    Update visualize.py

--- sub2node/visualize.py
+++ sub2node/visualize.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 
 from sklearn.manifold import TSNE
 from termcolor import cprint
@@ -45,6 +45,38 @@ def plot_data_points_by_tsne(xs: np.ndarray, ys: np.ndarray,
             plot_for_one_y(ys[:, y_idx], _key=f"{key}_y{y_idx}", _title=f"TSNE: {key} of y={y_idx}")
 
 
+def finish_plot(plot, kind,
+                xlabel, ylabel,
+                path, key, extension,
+                hue_name,
+                label_kws: Dict[str, Any],
+                scales_kws: Dict[str, Any],
+                xticks, yticks,
+                tight_layout_kwargs: dict,
+                **kwargs):
+    if label_kws is not None:
+        plot.set(**label_kws)  # e.g., xlabel=None
+    if scales_kws is not None:
+        plot.set(**scales_kws)  # e.g., xscale="log", yscale="log"
+    if yticks is not None:
+        plt.yticks(yticks)
+    if xticks is not None:
+        plt.xticks(xticks)
+
+    if tight_layout_kwargs is not None:
+        # e.g., rect=[0, 0, 0.8, 1]
+        plt.tight_layout(**tight_layout_kwargs)
+
+    plot_info = "_".join([k for k in [xlabel, ylabel, hue_name]
+                          if k is not None])
+    plot_info = plot_info.replace("/", "|").replace("#", "Num")
+    path_and_name = f"{path}/fig_{kind}_{key}_{plot_info}.{extension}"
+
+    plot.savefig(path_and_name, bbox_inches='tight')
+    cprint(f"Saved: {path_and_name}", "blue")
+    plt.clf()
+
+
 def plot_catplot(kind,
                  xs, ys, xlabel, ylabel,
                  path, key, extension="pdf",
@@ -73,27 +105,8 @@ def plot_catplot(kind,
         **kwargs,
     )
 
-    if label_kws is not None:
-        plot.set(**label_kws)  # e.g., xlabel=None
-    if scales_kws is not None:
-        plot.set(**scales_kws)  # e.g., xscale="log", yscale="log"
-    if yticks is not None:
-        plt.yticks(yticks)
-    if xticks is not None:
-        plt.xticks(xticks)
-
-    if tight_layout_kwargs is not None:
-        # e.g., rect=[0, 0, 0.8, 1]
-        plt.tight_layout(**tight_layout_kwargs)
-
-    plot_info = "_".join([k for k in [xlabel, ylabel, hue_name]
-                          if k is not None])
-    plot_info = plot_info.replace("/", "|").replace("#", "Num")
-    path_and_name = "{}/fig_{}_{}_{}.{}".format(path, kind, key, plot_info, extension)
-
-    plot.savefig(path_and_name, bbox_inches='tight')
-    cprint(f"Saved: {path_and_name}", "blue")
-    plt.clf()
+    finish_plot(plot, kind, xlabel, ylabel, path, key, extension, hue_name,
+                label_kws, scales_kws, xticks, yticks, tight_layout_kwargs)
 
 
 def plot_box(xs, ys, xlabel, ylabel,
@@ -179,27 +192,8 @@ def plot_relplot(kind,
         for lh in plot._legend.legendHandles:
             lh.set_sizes([kwargs["s"]])
 
-    if label_kws is not None:
-        plot.set(**label_kws)  # e.g., xlabel=None
-    if scales_kws is not None:
-        plot.set(**scales_kws)  # e.g., xscale="log", yscale="log"
-    if yticks is not None:
-        plt.yticks(yticks)
-    if xticks is not None:
-        plt.xticks(xticks)
-
-    if tight_layout_kwargs is not None:
-        # e.g., rect=[0, 0, 0.8, 1]
-        plt.tight_layout(**tight_layout_kwargs)
-
-    plot_info = "_".join([k for k in [xlabel, ylabel, hue_name, style_name]
-                          if k is not None])
-    plot_info = plot_info.replace("/", "|").replace("#", "Num")
-    path_and_name = f"{path}/fig_{kind}_{key}_{plot_info}.{extension}"
-
-    plot.savefig(path_and_name, bbox_inches='tight')
-    cprint(f"Saved: {path_and_name}", "blue")
-    plt.clf()
+    finish_plot(plot, kind, xlabel, ylabel, path, key, extension, hue_name,
+                label_kws, scales_kws, xticks, yticks, tight_layout_kwargs)
 
 
 def plot_scatter(xs, ys, xlabel, ylabel,
@@ -252,3 +246,108 @@ def plot_line(xs, ys, xlabel, ylabel,
                  xticks=xticks, yticks=yticks,
                  tight_layout_kwargs=tight_layout_kwargs,
                  **kwargs)
+
+
+def plot_dis(kind,
+             xs: Union[list, dict], xlabel,
+             path, key, extension="pdf",
+             ys: Union[list, dict] = None, ylabel=None,  # NOTE: y will be used for 2d dis-plot
+             hues=None, hue_name=None,
+             rows=None, row_name=None,
+             cols=None, col_name=None,
+             label_kws: Dict[str, Any] = None,
+             scales_kws: Dict[str, Any] = None,
+             xticks=None, yticks=None,
+             tight_layout_kwargs: dict = None,
+             **kwargs):
+    data = {
+        xlabel: xs,
+        # ylabel: ys,  NOTE: y will be used for 2d dis-plot
+        **{obj_name: obj for obj_name, obj in zip([ylabel, hue_name, col_name, row_name],
+                                                  [ys, hues, cols, rows])
+           if obj_name is not None}
+    }
+    df = pd.DataFrame(data)
+
+    plot = sns.displot(
+        kind=kind,
+        data=df,
+        x=xlabel, y=ylabel, hue=hue_name, row=row_name, col=col_name,
+        **kwargs,
+    )
+
+    finish_plot(plot, kind, xlabel, ylabel, path, key, extension, hue_name,
+                label_kws, scales_kws, xticks, yticks, tight_layout_kwargs)
+
+
+def from_counter(x_counter_list, y_counter_list, hues, rows, cols):
+    xs_list = [sum([[k] * v for k, v in x_counter.items()], []) for x_counter in x_counter_list]
+    xs = sum(xs_list, [])
+
+    ys = None
+    if y_counter_list is not None:
+        ys = sum([sum([[k] * v for k, v in y_counter.items()], []) for y_counter in y_counter_list], [])
+
+    if hues is not None:
+        assert len(hues) == len(x_counter_list)
+        hues = sum([[h] * len(xs) for xs, h in zip(xs_list, hues)], [])
+    if rows is not None:
+        assert len(rows) == len(x_counter_list)
+        rows = sum([[r] * len(xs) for xs, r in zip(xs_list, rows)], [])
+    if cols is not None:
+        assert len(cols) == len(x_counter_list)
+        cols = sum([[c] * len(xs) for xs, c in zip(xs_list, cols)], [])
+
+    return xs, ys, hues, rows, cols
+
+
+def plot_hist_from_counter(x_counter_list: List[dict], xlabel,
+                           path, key, extension="pdf",
+                           y_counter_list: List[dict] = None, ylabel=None,  # NOTE: y will be used for 2d dis-plot
+                           hues=None, hue_name=None,
+                           rows=None, row_name=None,
+                           cols=None, col_name=None,
+                           label_kws: Dict[str, Any] = None,
+                           scales_kws: Dict[str, Any] = None,
+                           xticks=None, yticks=None,
+                           tight_layout_kwargs: dict = None,
+                           **kwargs):
+    xs, ys, hues, rows, cols = from_counter(x_counter_list, y_counter_list, hues, rows, cols)
+
+    plot_dis("hist", xs=xs, xlabel=xlabel,
+             path=path, key=key, extension=extension,
+             ys=ys, ylabel=ylabel,
+             hues=hues, hue_name=hue_name,
+             rows=rows, row_name=row_name,
+             cols=cols, col_name=col_name,
+             label_kws=label_kws,
+             scales_kws=scales_kws,
+             xticks=xticks, yticks=yticks,
+             tight_layout_kwargs=tight_layout_kwargs,
+             **kwargs)
+
+
+def plot_kde_from_counter(x_counter_list: List[dict], xlabel,
+                          path, key, extension="pdf",
+                          y_counter_list: List[dict] = None, ylabel=None,  # NOTE: y will be used for 2d dis-plot
+                          hues=None, hue_name=None,
+                          rows=None, row_name=None,
+                          cols=None, col_name=None,
+                          label_kws: Dict[str, Any] = None,
+                          scales_kws: Dict[str, Any] = None,
+                          xticks=None, yticks=None,
+                          tight_layout_kwargs: dict = None,
+                          **kwargs):
+    xs, ys, hues, rows, cols = from_counter(x_counter_list, y_counter_list, hues, rows, cols)
+
+    plot_dis("kde", xs=xs, xlabel=xlabel,
+             path=path, key=key, extension=extension,
+             ys=ys, ylabel=ylabel,
+             hues=hues, hue_name=hue_name,
+             rows=rows, row_name=row_name,
+             cols=cols, col_name=col_name,
+             label_kws=label_kws,
+             scales_kws=scales_kws,
+             xticks=xticks, yticks=yticks,
+             tight_layout_kwargs=tight_layout_kwargs,
+             **kwargs)
