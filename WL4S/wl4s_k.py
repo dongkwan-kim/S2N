dadf1127a1a353b63d946888ddb30fb2fef0e3c3
Dongkwan Kim <todoaskit@gmail.com>
2024-06-21 16:59:07 +0900

    Fix an error

--- WL4S/wl4s_k.py
+++ WL4S/wl4s_k.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     Cx100 = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
     MORE_HPARAM_SPACE = {
         "C": [c / 100 for c in Cx100],
-        "dual": [True, False],
     }
 
     __args__ = parser.parse_args()
     __args__.stype = "separated"
+    __args__.dtype = "kernel"
     __args__.wl_cumcat = False
 
     MODE = "real"  # syn, real
