909b32c6785bb596ff79e45656cdb3daed39d4e0
Dongkwan Kim <todoaskit@gmail.com>
2024-06-22 01:30:39 +0900

    Refactoring

--- WL4S/wl4s_k.py
+++ WL4S/wl4s_k.py
@@ -7,9 +7,6 @@ if __name__ == '__main__':
         "stype": ["separated"],
         "wl_cumcat": [False],
         "hist_norm": [False, True],
-        "model": ["SVC"],
-        "kernel": ["precomputed"],
-        "dtype": ["kernel"],
     }
     Cx100 = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
     MORE_HPARAM_SPACE = {
@@ -18,12 +15,13 @@ if __name__ == '__main__':
 
     __args__ = parser.parse_args()
     __args__.stype = "separated"
-    __args__.dtype = "kernel"
     __args__.wl_cumcat = False
 
-    MODE = "real_precomputation"  # syn, real
+    MODE = "real_k"
 
     if MODE == "syn":
+        HPARAM_SPACE = {**HPARAM_SPACE, "model": ["LinearSVC"]}
+        MORE_HPARAM_SPACE = {**MORE_HPARAM_SPACE, "dual": [True, False]}
         for k_to_sample in [1, 2, 3, 4]:
             __args__.k_to_sample = k_to_sample
             kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample}")
@@ -31,21 +29,32 @@ if __name__ == '__main__':
                 __args__.dataset_name = dataset_name
                 hp_search_for_models(__args__, HPARAM_SPACE, MORE_HPARAM_SPACE, **kws)
 
-    elif MODE == "real_precomputation":
-        for k_to_sample in [None, 1, 2, 3, 4]:
-            for dataset_name in ["PPIBP", "EMUser"]:
-                __args__.k_to_sample = k_to_sample
-                __args__.dataset_name = dataset_name
-                kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample}")
-                for hist_norm in [False, True]:
-                    __args__.hist_norm = hist_norm
-                    precompute_all_kernels(__args__)
-                    gc.collect()
+    else:
+        HPARAM_SPACE = {**HPARAM_SPACE, "model": ["SVC"], "kernel": ["precomputed"], "dtype": ["kernel"]}
+        __args__.dtype = "kernel"
 
-    elif MODE == "real":
-        for k_to_sample in [None, 1, 2, 3, 4]:
+        if MODE == "real_precomputation":
+            for k_to_sample in [None, 1, 2, 3, 4]:
+                for dataset_name in ["PPIBP", "EMUser"]:
+                    __args__.k_to_sample = k_to_sample
+                    __args__.dataset_name = dataset_name
+                    for hist_norm in [False, True]:
+                        __args__.hist_norm = hist_norm
+                        precompute_all_kernels(__args__)
+                        gc.collect()
+
+        elif MODE == "real_k":
+            for k_to_sample in [None, 1, 2, 3, 4]:  # NOTE: [None, 1, 2, 3, 4]
+                for dataset_name in ["PPIBP", "EMUser"]:
+                    __args__.k_to_sample = k_to_sample
+                    __args__.dataset_name = dataset_name
+                    kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample or 0}")
+                    hp_search_for_models(__args__, HPARAM_SPACE, MORE_HPARAM_SPACE, **kws)
+
+        elif MODE == "real_k_inf":
+            __args__.stype = "connected"
+            HPARAM_SPACE["stype"] = ["connected"]
             for dataset_name in ["PPIBP", "EMUser"]:
-                __args__.k_to_sample = k_to_sample
                 __args__.dataset_name = dataset_name
-                kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample}")
+                kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_inf")
                 hp_search_for_models(__args__, HPARAM_SPACE, MORE_HPARAM_SPACE, **kws)
