ebb9ec0bf85961c4cf42c20d7bc8b266a3a0585e
Dongkwan Kim <todoaskit@gmail.com>
2024-06-24 22:27:53 +0900

    Update wl4s_k

--- WL4S/wl4s_k.py
+++ WL4S/wl4s_k.py
@@ -5,27 +5,26 @@ from wl4s import parser, hp_search_for_models, precompute_all_kernels
 if __name__ == '__main__':
     HPARAM_SPACE = {
         "stype": ["separated"],
-        "wl_cumcat": [False],
+        "wl_cumcat": [False, True],
         "hist_norm": [False, True],
     }
     Cx100 = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
     MORE_HPARAM_SPACE = {
         "C": [c / 100 for c in Cx100],
     }
-    DATA_TO_RATIO_SAMPLES = {"HPOMetab": 1800 / 2400, "HPONeuro": 1800 / 4000}
+    DATA_TO_RATIO_SAMPLES = {"HPOMetab": 1400 / 2400, "HPONeuro": 1400 / 4000}
 
     __args__ = parser.parse_args()
     __args__.stype = "separated"
-    __args__.wl_cumcat = False
 
     MODE = "real_large_precomputation"
 
     if MODE == "syn":
         HPARAM_SPACE = {**HPARAM_SPACE, "model": ["LinearSVC"]}
         MORE_HPARAM_SPACE = {**MORE_HPARAM_SPACE, "dual": [True, False]}
-        for k_to_sample in [1, 2, 3, 4]:
+        for k_to_sample in [None, 1, 2]:
             __args__.k_to_sample = k_to_sample
-            kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample}")
+            kws = dict(file_dir="../_logs_wl4s_k", log_postfix=f"_{k_to_sample or 0}")
             for dataset_name in ["Component", "Density", "Coreness", "CutRatio"]:
                 __args__.dataset_name = dataset_name
                 hp_search_for_models(__args__, HPARAM_SPACE, MORE_HPARAM_SPACE, **kws)
